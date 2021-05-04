ad151def61edc80f2a081cbcc2084c7c98588884
crogar <58400421+crogar@users.noreply.github.com>
2021-05-03 23:23:16 -0400

    changing file tree

--- static/js/app.js
+++ static/js/app.js
@@ -1,8 +1,19 @@
 // Use D3 fetch to read the JSON file
 // The data from the JSON file is arbitrarily named importedData as the argument
-d3.json("data/data.json").then((importedData) => {
-    // console.log(importedData);
-    var data = importedData;
+d3.json("./data/samples.json").then((data) => {
+    populate_dropdown(data.names);
+    
     console.log(data)
+});
+
+function populate_dropdown(names){   // This functions populates the dropdown menu
+  var options = d3.select("#selDataset");
+    names.forEach((name)=>{  
+      var option = options.append("option")
+      option.text(name).property("value",name)
   });
-  
+}
+
+function plot_bar(data){
+
+}
