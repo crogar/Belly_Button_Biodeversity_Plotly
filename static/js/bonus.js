d524abc4062f8bf65328346d0dd39f65a04d3ab5
crogar <58400421+crogar@users.noreply.github.com>
2021-05-05 20:32:54 -0400

    something changed

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,37 +1,46 @@
-d3.json("./data/samples.json").then((sample_data) => {
+function optionChanged(subject_id){
+  d3.json("./data/samples.json").then((sample_data) => {
     // console.log(data)
     var wfreq = sample_data.metadata.map(result => result.wfreq)
+    var max_value = Math.max( ...wfreq ); 
     for(var i=0;i<wfreq.length;i++){
-        // console.log(wfreq[i])
         if (wfreq[i] === null){ wfreq[i] = 0}
     }
-    for(var i=0;i<wfreq.length;i++){
-        console.log(wfreq[i])
+    console.log(parseInt(subject_id))
 
-    }
+    
+    var subject_wfreq = wfreq[parseInt(subject_id)];
+  
     var data = [
         {
-          domain: { x: [0, 1], y: [0, 1] },
-          value: 6,
-          title: { text: "Speed" },
+          domain: { x: [0, 2], y: [0, 2] },
+          value: wfreq[56],
+          title: { text: "WashFrequency" },
           type: "indicator",
           mode: "gauge+number",
           gauge: {
-            axis: { range: [null, 9] },
+            axis: { range: [0, max_value] },
             steps: [
-              { range: [0, 3], color: "lightgray" },
-              { range: [3, 6], color: "gray" }
+              { range: [0, 1], color: "lightgray" },
+              { range: [1, 2], color: "gray" },
+              { range: [2, 3], color: "gray" },
+              { range: [3, 4], color: "gray" },
+              { range: [4, 5], color: "gray" },
+              { range: [5, 6], color: "gray" },
+              { range: [6, 7], color: "gray" },
+              { range: [7, 8], color: "gray" },
+              { range: [8, 9], color: "gray" },
             ],
             threshold: {
               line: { color: "red", width: 4 },
               thickness: 0.75,
-              value: 6
+              value: subject_wfreq
             }
           }
         }
       ];
       
       var layout = { width: 600, height: 450, margin: { t: 0, b: 0 } };
-      Plotly.newPlot('gauge', data, layout);
-      
+      Plotly.newPlot('gauge', data, layout);   
 });
+}
